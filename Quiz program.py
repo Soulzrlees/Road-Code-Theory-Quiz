693bd7f79a59101e6f4a21c01b3b3fe9e79a318d
Shawn Lee <100383590+Soulzrlees@users.noreply.github.com>
2023-06-26 10:16:54 +1200

    Add files via upload
    Added all necessary images on the main interface (removed border for the mean time)

--- Quiz program.py
+++ Quiz program.py
@@ -17,21 +17,31 @@ def main_interface_label():
     Parking_Label = Label(root, text="Parking", height= 2, width=20).place(x=100, y=500)
     Road_Positions_Label = Label(root, text="Road Positions", height= 2, width=20).place(x=400, y=500)
     Sign_label = Label(root, text="Sign", height= 2, width=20).place(x=700, y=500)
-    Timelimit_label = Button(root, text="Time limit on / off", height= 2, width=20).place(x=1100, y=400)
-    RandomQuestion_label = Button(root, text="Random 10 Questions", height= 2, width=20).place(x=1100, y=300)
+    Timelimit_label = Button(root, text="Time limit on / off", height= 2, width=20).place(x=1150, y=250)
+    RandomQuestion_label = Button(root, text="Random 10 Questions", height= 2, width=20).place(x=1150, y=400)
 
 #Creating main interface image (that acts as a button) for each different categories
 def main_interface_image():
-    global behavior_picture, root, Emergency_picture, Parking_picture
+    global behavior_picture, root, Emergency_picture, Parking_picture, Intersection_picture, Road_Position_picture, Sign_picture
     behavior_picture = PhotoImage(file="Image_Folder/Main Image/Behaviormainpic.png").subsample(1,1)
-    Photo_behavior_picture_label =Button(root, image= behavior_picture, text="Behavior", relief="solid", bg= "black", activebackground="#f6f3a2", height = 187, width = 280)
+    Photo_behavior_picture_label =Button(root, image= behavior_picture, text="Behavior")
     Photo_behavior_picture_label.place(x=99, y=70)
     Emergency_picture = PhotoImage(file="Image_Folder/Main Image/Emergencymainpic.png").subsample(1,1)
-    Photo_Emergency_picture_label = Button(root, image= Emergency_picture, text="Emergency", borderwidth=5, relief="solid")
+    Photo_Emergency_picture_label = Button(root, image= Emergency_picture, text="Emergency")
     Photo_Emergency_picture_label.place(x=400, y=70)
+    Intersection_picture = PhotoImage(file="Image_Folder/Main Image/Intersectionmainpic.png").subsample(1,1)
+    Photo_Intersection_picture_label = Button(root, image= Intersection_picture, text="Intersection")
+    Photo_Intersection_picture_label.place(x=700, y=70)
     Parking_picture = PhotoImage(file="Image_Folder/Main Image/Parkingmainpic.png").subsample(1,1)
-    Photo_Parking_picture_label = Button(root, image= Parking_picture, text="Parking", borderwidth=5, relief="solid")
-    Photo_Parking_picture_label.place(x=700, y=70)
+    Photo_Parking_picture_label = Button(root, image= Parking_picture, text="Parking")
+    Photo_Parking_picture_label.place(x=99, y=350)
+    Road_Position_picture = PhotoImage(file="Image_Folder/Main Image/Road_Positionmainpic.png").subsample(1,1)
+    Photo_Road_Position_picture_label = Button(root, image= Road_Position_picture, text="Road_position")
+    Photo_Road_Position_picture_label.place(x=400, y=350)
+    Sign_picture = PhotoImage(file="Image_Folder/Main Image/Signmainpic.png").subsample(1,1)
+    Photo_Sign_picture_label = Button(root, image= Sign_picture, text="Sign")
+    Photo_Sign_picture_label.place(x=700, y=350)
+    
     
 # Main function to create a window
 def Main():
