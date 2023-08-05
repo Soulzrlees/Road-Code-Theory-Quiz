6f4d7c211ad8f9888bb0804e31876d9b1a6052a9
Shawn Lee <100383590+Soulzrlees@users.noreply.github.com>
2023-08-05 14:49:57 +1200

    Folder of quiz program

---
+++ Quiz program/Behaviour.py
@@ -0,0 +1,111 @@
+
+Q1B = {"Question": "What does a white diamond\n painted on the road mean?",
+
+    "Choices": ["There is a crest in the road",
+                "You are approaching a school entrance",
+                "The speed limit is about to change",
+                "You are approaching a pedestrian crossing"],
+
+    "Answer": "You are approaching a pedestrian crossing",
+
+    "Image": "Image_Folder/Question images/Behavior/Q1Bpic.png"}
+#_________________________________________________________________________________________________________________________________________________________
+
+Q2B = {"Question": "If you have to drive at a slow speed that\n may hold up other vehicles, what should you do?",
+
+    "Choices": ["drive at night when there's less traffic",
+                "Keep to the left and let others pass where possible",
+                "Nothing - drive as normal as it's other\n drivers' resposibility to overtake you safely",
+                "Keep as close as you can to the centre of the road"],
+
+    "Answer": "Keep to the left and let others pass where possible"}
+#_________________________________________________________________________________________________________________________________________________________
+
+Q3B = {"Question": "Where would you see this cat's eye?",
+
+    "Choices": ["left side of road",
+                "Centre line of road",
+                "No passing lane",
+                "Fire hydrant"],
+
+    "Answer": "Centre line of road",
+
+    "Image": "Image_Folder/Question images/Behavior/Q3Bpic.png"}
+#_________________________________________________________________________________________________________________________________________________________
+
+Q4B = {"Question": "What must you do when you come up to  a red light at traffic signal?",
+
+    "Choices": ["Stop and wait for a green light",
+                "Stop, unless you are turning left",
+                "Stop, unless you are turning right",
+                "Stop, then proceed if the way is clear"],
+
+    "Answer": "Stop and wait for a green light",
+
+    "Image": "Image_Folder/Question images/Behavior/Q4Bpic.png"}
+#_________________________________________________________________________________________________________________________________________________________
+
+Q5B = {"Question": "All the following are ways you can check for\n hazards before reversing. What's the best way, though?",
+
+    "Choices": ["Check your wing mirrors and rear-view mirror",
+                "Look over your shoulder",
+                "Get out and walk around the vehicle",
+                " Be sure any reversing sensors or cameras\n are activated(if your vehicle has them)"],
+
+    "Answer": "Get out and walk around the vehicle"}
+#_________________________________________________________________________________________________________________________________________________________
+
+Q6B = {"Question": "You are driving downhill on a steep, narrow road\n and a vehicle is coming up the hill. What should you do?",
+
+    "Choices": ["Vehicles moving downhill should\n give way to vehicles moving uphill",
+                "Sound your horn and signal the\n other vheicle to move out of the way",
+                "Carry on - the other vehicle\n will move out of the way",
+                "Flash your headlights to\n determine who goes first"],
+
+    "Answer": "Vehicles moving downhill should\n give way to vehicles moving uphill"}
+#_________________________________________________________________________________________________________________________________________________________
+
+Q7B = {"Question": "You are the driver of the blue car. Of the four hazards\n marked, which one is most likely to require you to take urgent action?",
+
+    "Choices": ["The car turning left onto the other side of the road",
+                "The car coming towards you",
+                "The car you are following",
+                "The car on the other side of the road turning right"],
+
+    "Answer": "The car on the other side of the road turning right",
+
+    "Image": "Image_Folder/Question images/Behavior/Q7Bpic.png"}
+#_________________________________________________________________________________________________________________________________________________________
+
+Q8B = {"Question": "What is the safest way to carry goods inside a vehicle?",
+
+    "Choices": ["In the boot",
+                "On the back seat",
+                "Held by a passenger",
+                "On your lap"],
+
+    "Answer": "In the boot"}
+#_________________________________________________________________________________________________________________________________________________________
+
+Q9B = {"Question": "What does this sign mean?",
+
+    "Choices": ["The left side of a bridge",
+                "The right side of a bridge",
+                "The left side of a road",
+                "The right side of a road"],
+
+    "Answer": "The right side of a bridge",
+
+    "Image": "Image_Folder/Question images/Behavior/Q9Bpic.png"}
+#_________________________________________________________________________________________________________________________________________________________
+
+Q10B = {"Question": "How far can your load extend either side of the centre line of your vehicle?",
+
+    "Choices": ["1m",
+                "1.275m",
+                "1.5m",
+                "1.75m"],
+
+    "Answer": "1.275m",
+
+    "Image": "Image_Folder/Question images/Behavior/Q10Bpic.png"}
---
+++ Quiz program/Intersections.py
@@ -0,0 +1,100 @@
+Q1I = {"Question": "Does the driver of the blue car have to give way?",
+
+    "Choices": ["Yes",
+                "No"],
+
+    "Answer": "No",
+
+    "Image": "Image_Folder/Question images/Intersections/Q1Ipic.png"}
+#_________________________________________________________________________
+
+Q2I = {"Question": "Does the driver of the blue car have to give way?",
+
+    "Choices": ["Yes",
+                "No"],
+
+    "Answer": "Yes",
+
+    "Image": "Image_Folder/Question images/Intersections/Q2Ipic.png"}
+#____________________________________________________________________________
+
+Q3I = {"Question": "Does the driver of the blue car have to give way?",
+
+    "Choices": ["Yes",
+                "No"],
+
+    "Answer": "Yes",
+
+    "Image": "Image_Folder/Question images/Intersections/Q3Ipic.png"}
+#__________________________________________________________________________
+
+Q4I = {"Question": "You're approaching this roundabout and\n want to follow the arrow. Do you need to indicate?",
+
+    "Choices": ["Yes-indicate left as you approach\n the roundabout and until you take the exit",
+                "No-it's almost a straigth line and\n therefore doesn't need an indicator",
+                "Yes, indicate left as soon\n as you're on the roundabout",
+                "None of the above"],
+
+    "Answer": "Yes-indicate left as you approach\n the roundabout and until you take the exit",
+
+    "Image": "Image_Folder/Question images/Intersections/Q4Ipic.png"}
+#____________________________________________________________________________________
+
+Q5I = {"Question": "Whats; the most important rule for approaching an intersection",
+
+    "Choices": ["Indicate for atleast a second so that\n other road users know where you are going",
+                "Be prepared to stop",
+                "Use your horn to warn other traffic",
+                "Change lane at the last minute so that you\n end up in the one with the shortest queue"],
+
+    "Answer": "Be prepared to stop"}
+#___________________________________________________________________________________________
+
+Q6I = {"Question": "You are driving the orange car through an\n uncontrolled intersection. Do you have to indicate?",
+
+    "Choices": ["Yes",
+                "No"],
+
+    "Answer": "No",
+
+    "Image": "Image_Folder/Question images/Intersections/Q6Ipic.png"}
+#____________________________________________________________________________________________
+
+Q7I = {"Question": "Does the driver of the blue car have to give way?",
+
+    "Choices": ["Yes",
+                "No"],
+
+    "Answer": "Yes",
+
+    "Image": "Image_Folder/Question images/Intersections/Q7Ipic.png"}
+#________________________________________________________________________________________
+
+Q8I = {"Question": "What must you do at an intersection controlled by a stop sign?",
+
+    "Choices": ["Always come to a complete stop",
+                "Only stop if there are other vehciles coming",
+                "ONly stop if there are traffic lights as well",
+                "None of the above"],
+
+    "Answer": "Always come to a complete stop"}
+#_________________________________________________________________________________________________
+
+Q9I = {"Question": "You're approaching an uncontrolled intersection with no road\n markings or lights and you want to turn left. Do you need to indicate?",
+
+    "Choices": ["Yes",
+                "No"],
+
+    "Answer": "Yes",
+
+    "Image": "Image_Folder/Question images/Intersections/Q9Ipic.png"}
+#_________________________________________________________________________________________________
+
+Q10I = {"Question": "Does the driver of the blue car have to give way?",
+
+    "Choices": ["Yes",
+                "No"],
+
+    "Answer": "Yes",
+
+    "Image": "Image_Folder/Question images/Intersections/Q10Ipic.png"}
